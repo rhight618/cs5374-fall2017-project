4f6796ccd2c1e4108f2b78e086fc190b0600850e
Hightower <rhight618@gmail.com>
2017-10-24 16:34:10 -0500

    changed code to point to new feature file

--- src/test/java/net/continuumsecurity/junit/WebGoatSecurityTest.java
+++ src/test/java/net/continuumsecurity/junit/WebGoatSecurityTest.java
@@ -10,11 +10,11 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = {
-                "src/test/resources/features/"
+                "src/test/resources/webGoatFeatures/"
         },
         format = {"pretty", "html:build/reports/cucumber/html", "json:build/reports/cucumber/all_tests.json", "junit:build/reports/junit/all_tests.xml"},
         glue = {"net.continuumsecurity.steps"},
-        tags = {"~@skip","~@app_scan","~@cors","~@authentication","@authorisation","~@data_security","~@host_config","~@http_headers","~@nessus_scan","~@passive_scan","~@session_management","~@ssl"}
+        tags = {"~@skip", "login"}
 )
 public class WebGoatSecurityTest {
 
